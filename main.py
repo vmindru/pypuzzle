7eed4e56bb74528a6fad4752e4aab67512e80e78
vmindru <vmindru@localhost.localdomain>
2014-04-19 08:43:04 -0400

    save

--- main.py
+++ main.py
@@ -6,15 +6,22 @@ import gtk
 class Base:
 		def __init__(puzz):
 			puzz.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-			puzz.window.show() 
 			puzz.window.set_border_width(200)	
-#			puzz.window.set_border_height(300)
+			puzz.button=gtk.Button("testme")	
+			puzz.button.connect("clicked",puzz.hello,None)	
+			puzz.window.add(puzz.button)
+			puzz.window.show() 
+			puzz.button.show()			
+
+	
 		def main(puzz):
 			gtk.main()
+		
+		def hello(puzz, widget, data=None):
+			print "testme data"
 
 if __name__ == "__main__" :
 	base = Base()
 	base.main()
-	time.sleep(5)
 
 
