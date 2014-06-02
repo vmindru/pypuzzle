616d0d58a16cbd729bcbaac7474bd01513b77d02
Veaceslav Mindru <vmindru@vmindru.corp.netledger.com>
2014-06-02 20:05:10 +0200

    .

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
-#!/usr/bin/python 
+#!/usr/bin/python
 
 import pygame
 
+screen_x=640
+screen_y=480
 pygame.init()
-screen = pygame.display.set_mode((640, 480)) 
+screen = pygame.display.set_mode((screen_x, screen_y))
+
 
 class Game(object):
 	def main(self, screen):
+
+		image_x = 120
+		image_y = 240
+		image = pygame.image.load('apple.png')
+		clock = pygame.time.Clock()
+
 		while 1:
+			clock.tick(30)
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					return
 				if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
 					return
+			screen.fill((200, 200, 200))
+			print "spwaning at %i %i" % (image_x, image_y)
+			screen.blit(image, (image_x, image_y))
+			pygame.display.flip()
+
+class Draw(Game):
+	pass
+
+
 
 if __name__ == '__main__':
 	pygame.init()
 	screen = pygame.display.set_mode((640, 480))
-	Game().main(screen)
+	Draw().main(screen)
