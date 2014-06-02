d02d3df948b201df95e256927ff36176d858f87a
Veaceslav Mindru <vmindru@vmindru.corp.netledger.com>
2014-06-02 12:52:13 +0200

    1

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python 
+
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((640, 480)) 
+
+class Game(object):
+	def main(self, screen):
+		while 1:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					return
+				if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+					return
+
+if __name__ == '__main__':
+	pygame.init()
+	screen = pygame.display.set_mode((640, 480))
+	Game().main(screen)
